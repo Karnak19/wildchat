c26d9c1ee1cc5a537be487526dbf1b9cb1c8cc70
Basile <basile64.v@gmail.com>
2019-03-01 13:37:07 +0100

    bootstraped, added event on enter key pressing,

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 $(function() {
-   var socket = io.connect("http://192.168.88.27:3000");
+   var socket = io.connect("http://localhost:3000");
 
    var message = $("#message");
    var username = $("#username");
@@ -11,13 +11,35 @@ $(function() {
       socket.emit("new_message", { message: message.val() });
    });
 
+   sendMessage.click(function() {
+      message.val("");
+   });
+
+   message.bind("enterKey", function(e) {
+      socket.emit("new_message", { message: message.val() });
+   });
+   message.bind("enterKey", function(e) {
+      message.val("");
+   });
+
+   message.keyup(function(e) {
+      if (e.keyCode == 13) {
+         $(this).trigger("enterKey");
+      }
+   });
+
    socket.on("new_message", data => {
       console.log(data);
-      chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+      chatroom.append("<p class='alert alertt alert-secondary col-lg-10 offset-lg-1 col-8 offset-2'>" + data.username + ": " + data.message + "</p>");
    });
 
    sendUsername.click(function() {
       console.log(username.val());
       socket.emit("change_username", { username: username.val() });
+      chatroom.append("<p class='alert alert-warning col-lg-10 offset-lg-1 col-8 offset-2'>" + username.val() + " changes his pseudo</p>");
+   });
+
+   sendUsername.click(function() {
+      username.val("");
    });
 });
